4a0e5b1e5c5f8e66903bb9ece7b420d3c366278e
ryan-wsl <ryanef39@gmail.com>
2024-02-24 16:19:36 -0500

    rf-aws-vpc add

--- locals.tf
+++ locals.tf
@@ -7,12 +7,21 @@ locals {
       gitignore_template = "Terraform"
       visibility = "public"
     }
+
     "rf-terraform-tfe" = {
       name = "tf-terraform-tfe"
       owner = "ryanef"
       description = "automation repo for tfe"
       gitignore_template = "Terraform"
       visibility = "public"
+    }
+
+    "rf-aws-vpc" = {
+      name = "tf-terraform-tfe"
+      owner = "ryanef"
+      description = "automation repo for tfe"
+      gitignore_template = "Terraform"
+      visibility = "public"    
     }
   }
 }
